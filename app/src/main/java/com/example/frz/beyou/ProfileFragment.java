184a703f2a73d5f137286a5f9c1e7e22179dd0e7
kevin <kpruiz11@gmail.com>
2016-11-26 15:24:09 -0500

    java

--- app/src/main/java/com/example/frz/beyou/ProfileFragment.java
+++ app/src/main/java/com/example/frz/beyou/ProfileFragment.java
@@ -3,6 +3,7 @@ package com.example.frz.beyou;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,17 +18,18 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_user_profile, container, false);
-        //v.findViewById(R.id.button).setOnClickListener(this);
+        v.findViewById(R.id.afiliationid).setOnClickListener(this);
         return v;
     }
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.button:
-
+            case R.id.afiliationid:
+                Log.i("mensaje","ggwp");
                 getActivity().getSupportFragmentManager().beginTransaction()
                         .setCustomAnimations(R.anim.zoom_in,R.anim.zoom_out)
-                        .add(R.id.fragment,new android.support.v4.app.Fragment()).commit();
+                        .add(R.id.fragment,new AfiliationFragment()).addToBackStack(null).commit();
+                Log.i("mensaje","ggwp2");
 
                 break;
         }
